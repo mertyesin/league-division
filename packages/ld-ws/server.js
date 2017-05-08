51a04d585d6b0ff1008a1451574bc877be778c94
mertyesin <gs1905_mmy>
2017-05-08 19:31:27 +0300

    ws added

---
+++ packages/ld-ws/server.js
@@ -0,0 +1,22 @@
+"use strict";
+var express = require("express");
+var cors = require("cors");
+var matchResults = require("../../match-results.json");
+var app = express();
+app.use(cors());
+app.get("/match-result/", function (httpRequest, httpResponse) {
+    if (matchResults) {
+        sendResponse(httpResponse, matchResults);
+    }
+    else {
+        var newErr = "error details: match-results not found";
+        sendErrorResponse(httpResponse, 500, newErr);
+    }
+});
+function sendResponse(httpResponse, response) {
+    httpResponse.send(response);
+}
+function sendErrorResponse(httpResponse, httpErrorCode, errorMessage) {
+    httpResponse.status(httpErrorCode).send({ errorMessage: errorMessage });
+}
+app.listen(process.argv[2]);
---
+++ packages/ld-ws/server.ts
@@ -0,0 +1,29 @@
+import * as express from "express";
+import {Application, Request, Response} from "express";
+import {MatchResult} from "../ld-web/src/model/";
+import * as cors from "cors";
+
+const matchResults: MatchResult[] = require("../../match-results.json");
+const app: Application = express();
+app.use(cors());
+
+app.get("/match-result/", function(httpRequest: Request, httpResponse: Response) {
+
+    if (matchResults) {
+        sendResponse(httpResponse, matchResults);
+    }
+    else {
+        const newErr = `error details: match-results not found`;
+        sendErrorResponse(httpResponse, 500, newErr);
+    }
+});
+
+function sendResponse(httpResponse: Response, response: Object): void {
+    httpResponse.send(response);
+}
+
+function sendErrorResponse(httpResponse: Response, httpErrorCode: number, errorMessage: string): void {
+    httpResponse.status(httpErrorCode).send({errorMessage: errorMessage});
+}
+
+app.listen(process.argv[2]);
