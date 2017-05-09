d8452d4f80348ba8aaad4379879ca66306e764a9
mertyesin <gs1905_mmy>
2017-05-09 16:24:32 +0300

    generating matches with script is in progress!

---
+++ packages/ld-web/test/test.ts
@@ -0,0 +1,15 @@
+// const teamName: string = "Watford";
+// if(t.name === teamName){
+//     for(let i=0; i<38; i++){
+//         if(teamMatchResults[i].homeTeamName === teamName){
+//             if(teamMatchResults[i].homeGoals > teamMatchResults[i].awayGoals){
+//                 count++;
+//             }
+//         }else if(teamMatchResults[i].awayTeamName === teamName){
+//             if(teamMatchResults[i].awayGoals > teamMatchResults[i].homeGoals){
+//                 count++;
+//             }
+//         }
+//     }
+//     console.log(teamName + " is winner of " + count + " matches !");
+// }
