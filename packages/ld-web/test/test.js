cf94f97f3c84465925987b5ecc720be1b2e09249
mertyesin <gs1905_mmy>
2017-05-10 16:19:11 +0300

    ref

--- packages/ld-web/test/test.js
+++ packages/ld-web/test/test.js
@@ -1,15 +1,58 @@
-// const teamName: string = "Watford";
-// if(t.name === teamName){
-//     for(let i=0; i<38; i++){
-//         if(teamMatchResults[i].homeTeamName === teamName){
-//             if(teamMatchResults[i].homeGoals > teamMatchResults[i].awayGoals){
-//                 count++;
-//             }
-//         }else if(teamMatchResults[i].awayTeamName === teamName){
-//             if(teamMatchResults[i].awayGoals > teamMatchResults[i].homeGoals){
-//                 count++;
-//             }
-//         }
-//     }
-//     console.log(teamName + " is winner of " + count + " matches !");
-// }
+"use strict";
+var assert = require("assert");
+var props_factory_1 = require("../src/props-factory");
+var teams = require("../../ld-tools/generate/teams.json"), matchResults = require("../../../tmp/match-results.json");
+describe("match results - ", function () {
+    it("difference between GF and GA", function () {
+        var divisionTableProps = props_factory_1.createDivisionTableProps(teams, matchResults), diff = divisionTableProps.teamStatusList[0].goalsFor - divisionTableProps.teamStatusList[0].goalsAgainst;
+        assert.strictEqual(divisionTableProps.teamStatusList[0].difference, diff);
+    });
+    it("second difference calculation between GF and GA", function () {
+        var divisionTableProps = props_factory_1.createDivisionTableProps(teams, matchResults), diff = divisionTableProps.teamStatusList[7].goalsFor - divisionTableProps.teamStatusList[7].goalsAgainst;
+        assert.strictEqual(divisionTableProps.teamStatusList[7].difference, diff);
+    });
+    it("third difference calculation between GF and GA", function () {
+        var divisionTableProps = props_factory_1.createDivisionTableProps(teams, matchResults), diff = divisionTableProps.teamStatusList[12].goalsFor - divisionTableProps.teamStatusList[12].goalsAgainst;
+        assert.strictEqual(divisionTableProps.teamStatusList[12].difference, diff);
+    });
+    it("win count calculator", function () {
+        var winCounter = 0;
+        var divisionTableProps = props_factory_1.createDivisionTableProps(teams, matchResults);
+        for (var i = 0; i < 379; i++) {
+            if (matchResults[i].homeTeamName === "Liverpool") {
+                if (matchResults[i].homeGoals > matchResults[i].awayGoals) {
+                    winCounter++;
+                }
+            }
+            else if (matchResults[i].awayTeamName === "Liverpool") {
+                if (matchResults[i].awayGoals > matchResults[i].homeGoals) {
+                    winCounter++;
+                }
+            }
+        }
+        assert.strictEqual(divisionTableProps.teamStatusList.filter(function (t) { return t.name === "Liverpool"; })[0].win, winCounter);
+    });
+    it("point calculator", function () {
+        var winCounter = 0, drawnCounter = 0;
+        var divisionTableProps = props_factory_1.createDivisionTableProps(teams, matchResults);
+        for (var i = 0; i < 379; i++) {
+            if (matchResults[i].homeTeamName === "Chelsea") {
+                if (matchResults[i].homeGoals > matchResults[i].awayGoals) {
+                    winCounter++;
+                }
+                else if (matchResults[i].homeGoals === matchResults[i].awayGoals) {
+                    drawnCounter++;
+                }
+            }
+            else if (matchResults[i].awayTeamName === "Chelsea") {
+                if (matchResults[i].awayGoals > matchResults[i].homeGoals) {
+                    winCounter++;
+                }
+                else if (matchResults[i].homeGoals === matchResults[i].awayGoals) {
+                    drawnCounter++;
+                }
+            }
+        }
+        assert.strictEqual(divisionTableProps.teamStatusList.filter(function (t) { return t.name === "Chelsea"; })[0].points, winCounter * 3 + drawnCounter);
+    });
+});
--- packages/ld-web/test/test.ts
+++ packages/ld-web/test/test.ts
@@ -1,16 +1,97 @@
-// const teamName: string = "Watford";
-// if(t.name === teamName){
-//     for(let i=0; i<38; i++){
-//         if(teamMatchResults[i].homeTeamName === teamName){
-//             if(teamMatchResults[i].homeGoals > teamMatchResults[i].awayGoals){
-//                 count++;
-//             }
-//         }else if(teamMatchResults[i].awayTeamName === teamName){
-//             if(teamMatchResults[i].awayGoals > teamMatchResults[i].homeGoals){
-//                 count++;
-//             }
-//         }
-//     }
-//     console.log(teamName + " is winner of " + count + " matches !");
-// }
+import * as assert from "assert";
+import {Team} from "../src/model";
+import {MatchResult} from "../src/model";
 
+import {createDivisionTableProps} from "../src/props-factory";
+
+const teams: Team[] = require("../../ld-tools/generate/teams.json"),
+    matchResults: MatchResult[] = require("../../../tmp/match-results.json");
+
+describe("match results - ", function() {
+    it("difference between GF and GA", function () {
+
+        const divisionTableProps = createDivisionTableProps(teams, matchResults),
+            diff = divisionTableProps.teamStatusList[0].goalsFor - divisionTableProps.teamStatusList[0].goalsAgainst;
+
+        assert.strictEqual(
+            divisionTableProps.teamStatusList[0].difference,
+            diff
+        );
+    });
+
+    it("second difference calculation between GF and GA", function () {
+
+        const divisionTableProps = createDivisionTableProps(teams, matchResults),
+            diff = divisionTableProps.teamStatusList[7].goalsFor - divisionTableProps.teamStatusList[7].goalsAgainst;
+
+        assert.strictEqual(
+            divisionTableProps.teamStatusList[7].difference,
+            diff
+        );
+    });
+
+    it("third difference calculation between GF and GA", function () {
+
+        const divisionTableProps = createDivisionTableProps(teams, matchResults),
+            diff = divisionTableProps.teamStatusList[12].goalsFor - divisionTableProps.teamStatusList[12].goalsAgainst;
+
+        assert.strictEqual(
+            divisionTableProps.teamStatusList[12].difference,
+            diff
+        );
+    });
+
+    it("win count calculator", function () {
+        let winCounter: number = 0;
+
+        const divisionTableProps = createDivisionTableProps(teams, matchResults);
+
+        for(let i=0; i<379; i++){
+            if(matchResults[i].homeTeamName === "Liverpool"){
+                if(matchResults[i].homeGoals > matchResults[i].awayGoals){
+                    winCounter++;
+                }
+            }else if(matchResults[i].awayTeamName === "Liverpool"){
+                if(matchResults[i].awayGoals > matchResults[i].homeGoals){
+                    winCounter++;
+                }
+            }
+        }
+
+        assert.strictEqual(
+            divisionTableProps.teamStatusList.filter(t => t.name === "Liverpool")[0].win,
+            winCounter
+        );
+    });
+
+    it("point calculator", function () {
+        let winCounter: number = 0,
+            drawnCounter: number = 0;
+
+        const divisionTableProps = createDivisionTableProps(teams, matchResults);
+
+        for(let i=0; i<379; i++){
+            if(matchResults[i].homeTeamName === "Chelsea"){
+                if(matchResults[i].homeGoals > matchResults[i].awayGoals){
+                    winCounter++;
+                }
+                else if(matchResults[i].homeGoals === matchResults[i].awayGoals){
+                    drawnCounter++;
+                }
+            }else if(matchResults[i].awayTeamName === "Chelsea"){
+                if(matchResults[i].awayGoals > matchResults[i].homeGoals){
+                    winCounter++;
+                }
+                else if(matchResults[i].homeGoals === matchResults[i].awayGoals){
+                    drawnCounter++;
+                }
+            }
+        }
+
+        assert.strictEqual(
+            divisionTableProps.teamStatusList.filter(t => t.name === "Chelsea")[0].points,
+            winCounter * 3 + drawnCounter
+        );
+    });
+
+});
