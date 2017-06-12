ada279e39e87897574e7bd5f11060687afe4d878
Mustafa Mert Yeşin <mertyesin89@gmail.com>
2017-06-12 14:10:49 +0300

    Create removeLettersFromString.ts

--- training/100617/removeLettersFromString.ts
+++ training/100617/removeLettersFromString.ts
@@ -1,17 +1,3 @@
-/*
-    Kod -Typescript- isimli javascript'te type-check'in daha kolay yapılabilmesini sağlamak amacıyla kullanılan bir süper-set'te
-    yazıldı. Ts kodu compile edildiğinde
-
-
-
-*/
-/*
-  Question Explanation =>
-      Tekrar sayısı “2” verildiğinde, ‘a’,‘b’ ve ‘u’ harfleri çıkarılacak, sonuç: “koq x”
-      Tekrar sayısı “3” verildiğinde, ‘a’ harfi çıkarılacak, sonuç: “b kouq bux”
-      Tekrar sayısı “4” verildiğinde, hiç bir harf çıkarılmayacak, sonuç: “aaba kouq bux”
- */
-
 function removeLettersFromString(str: string, n: number): string {
     var obj = {}; // This object will keep each of the count of string elements. like => { a: 3, b: 2, k: 1, o: 1, u: 2, q: 1, x: 1 }
 
