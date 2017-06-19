4b830a9b65f82ec7ac938e6c76c0732561856053
Mustafa Mert Yeşin <mertyesin89@gmail.com>
2017-06-19 11:54:24 +0300

    Update removeLetters.js

--- training/100617/removeLetters.js
+++ training/100617/removeLetters.js
@@ -1,17 +1,3 @@
-/*
- 1- Kodu nodeJs yüklü bir bilgisayar'da ->
-    - $ node removeLetters -  komutuyla terminal'de çalıştırabilirsiniz.
-
- 2- Chrome konsoluna copy-paste yaparak çalıştırabilirsiniz.
-*/
-
-/*
- Question Explanation =>
-     Tekrar sayısı “2” verildiğinde, ‘a’,‘b’ ve ‘u’ harfleri çıkarılacak, sonuç: “koq x”
-     Tekrar sayısı “3” verildiğinde, ‘a’ harfi çıkarılacak, sonuç: “b kouq bux”
-     Tekrar sayısı “4” verildiğinde, hiç bir harf çıkarılmayacak, sonuç: “aaba kouq bux”
- */
-
 function removeLetters(str /*: string */, n /*: number */) {
     var obj = {}; // This object will keep each of the count of string elements. like => { a: 3, b: 2, k: 1, o: 1, u: 2, q: 1, x: 1 }
 
@@ -40,11 +26,6 @@ function removeLetters(str /*: string */, n /*: number */) {
 
     return output;
 }
-
-// Your Test Cases
-console.log(removeLetters("aaba kouq bux",2)); // returns " koq x"
-console.log(removeLetters("aaba kouq bux",3)); // returns "b kouq bux"
-console.log(removeLetters("aaba kouq bux",4)); // returns "aaba kouq bux"
 
 // My Test Cases
 console.log(removeLetters("cccva hngjjk",2)); // returns "va hngk"
